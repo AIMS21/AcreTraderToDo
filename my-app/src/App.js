f89207dd9df351adb604d67178b60495b713c13d
AIMS21 <85468743+AIMS21@users.noreply.github.com>
2022-07-28 01:52:58 -0500

    toggle and buttons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
 import Header from './Components/Header'
 import AddTask from './Components/AddTask';
 import ToDoList from './Components/ToDoList';
 
 function App() {
+  const currentUser = 1;
   return (
     <div>
       <Header />
       <AddTask />
-      <ToDoList />
+      <ToDoList currentUser={currentUser}/>
     </div>
   );
 }
--- my-app/src/Components/ToDoList.js
+++ my-app/src/Components/ToDoList.js
@@ -1,16 +1,42 @@
 import tasks from '../data.json'
+import Switch from "react-switch";
 
-function ToDoList() {
-    return (
-      <div>
-        {tasks && tasks.map(({ id, title, completed}) => (
-          <div key={id} className="row">
-            <p>{id} {title} {String(completed)}</p>
-            
-          </div>
-        ))}
-      </div>
-    );
+const ToDoList = (props) => {
+  const currentUser = props.currentUser;
+  let count = 0;
+  const change = () => {
+    this.completed = true
+    console.log("CHANGE")
+  }
+  const remove = () => {
+    console.log("REMOVE")
+  }
+  return (
+    <div>
+      <h3>User {currentUser} To Do List</h3>
+      <ul>
+      {tasks.sort(function(x, y) { return x.completed - y.completed }).map(({ userId, id, title, completed}) => {
+        if (currentUser === userId) {
+          count = count + 1;
+          return (
+            <li>
+              <div key={id} className="row">
+                <p>Task Number: {id} - {title}<Switch onChange={change} checked={completed}></Switch></p> 
+                <button onClick={remove}>remove</button>
+              </div>
+            </li>
+          );
+        }
+        else {
+          return (null)
+        }
+
+      })}
+      </ul>
+      {count === 0 ? <p>No current tasks for you right now!</p> : null}
+      
+    </div>
+  );
   }
   
   export default ToDoList;
